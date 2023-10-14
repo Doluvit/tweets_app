1a34735de59c395fdc0d46c6a94459202bbbb0ef
Doluvit <doluvit@gmail.com>
2023-10-14 19:17:23 +0300

    fetching users added

--- src/components/TweetCard/TweetCard.jsx
+++ src/components/TweetCard/TweetCard.jsx
@@ -1,13 +1,89 @@
-import { Card, CardBox, Hero, Logo } from './TweetCard.styled';
-import logo from '../../assets/img/logo.png'
-import picture from '../../assets/img/picture.png'
+import {
+  Card,
+  CardBox,
+  FollowButton,
+  Hero,
+  Logo,
+  UserAvatar,
+  UserAvatarBorder,
+  UserAvatarContainer,
+  UserAvatarWrapper,
+  UserInfoItem,
+  UserInfoWrapper,
+} from './TweetCard.styled';
+import logo from '../../assets/img/logo.png';
+import picture from '../../assets/img/picture.png';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const TweetCard = () => {
+  const [isFollowing, setIsFollowing] = useState(false);
+  const [users, setUsers] = useState(false);
+  const [followersQuery, setFollowersQuery] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get('https://652a9ed64791d884f1fd27e6.mockapi.io/tweets/users')
+      .then(function (response) {
+        setUsers(response.data[1]);
+        setFollowersQuery(response.data[1].followers);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  const togglefollow = () => {
+   
+    if (isFollowing) {
+      setFollowersQuery(followersQuery - 1);
+      axios
+        .put(
+          `https://652a9ed64791d884f1fd27e6.mockapi.io/tweets/users/${users.id}`,
+          { followers: followersQuery - 1 }
+        )
+        .then(function (response) {
+          console.log('added');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    } else {
+      setFollowersQuery(followersQuery + 1);
+      axios
+        .put(
+          `https://652a9ed64791d884f1fd27e6.mockapi.io/tweets/users/${users.id}`,
+          { followers: followersQuery + 1 }
+        )
+        .then(function (response) {
+          console.log('ok');
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <CardBox>
       <Card>
-              <Logo src={logo} alt="img" />
-              <Hero src={picture} alt="ex" />
+        <Hero src={picture} alt="ex" />
+        <Logo src={logo} alt="img" />
+        <UserAvatarWrapper>
+          <UserAvatarBorder>
+            <UserAvatarContainer>
+              <UserAvatar src={users.avatar} />
+            </UserAvatarContainer>
+          </UserAvatarBorder>
+        </UserAvatarWrapper>
+        <UserInfoWrapper>
+          <UserInfoItem> {users.tweets} tweets</UserInfoItem>
+          <UserInfoItem>{followersQuery} followers</UserInfoItem>
+        </UserInfoWrapper>
+        <FollowButton onClick={togglefollow} isFollowing={isFollowing}>
+          {isFollowing ? 'following' : 'follow'}
+        </FollowButton>
       </Card>
     </CardBox>
   );
--- src/components/TweetCard/TweetCard.styled.js
+++ src/components/TweetCard/TweetCard.styled.js
@@ -1,30 +1,139 @@
-import styled from "styled-components";
+import styled, { css } from 'styled-components';
 
-
-export const CardBox = styled.div`
-display: flex;
-justify-content: center;
-`
-
-
-export const Card = styled.div`
-  width: 380px;
-  height: 200px;
-  background: #471ca9;
+export const CardBox = styled.li`
   display: flex;
   justify-content: center;
+`;
+
+export const Card = styled.div`
   position: relative;
+  padding: 28px 36px;
+  background: linear-gradient(
+    115deg,
+    #471ca9 -0.99%,
+    #5736a3 54.28%,
+    #4b2a99 78.99%
+  );
+  box-shadow: -2.5777px 6.87386px 20.62157px 0px rgba(0, 0, 0, 0.23);
+  /* display: flex; */
+  justify-content: center;
   border-radius: 20px;
+
+  &::after {
+    position: absolute;
+    right: 0;
+    top: 50%;
+
+    content: '';
+    height: 8px;
+    width: 100%;
+    flex-shrink: 0;
+
+    background: #ebd8ff;
+    box-shadow: 0px 3.43693px 2.5777px 0px #fbf8ff inset,
+      0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.06),
+      0px -1.71846px 3.43693px 0px #ae7be3 inset;
+
+    transform: translate(0, -50%);
+  }
 `;
 
 export const Logo = styled.img`
-position: absolute;
-left: 20px;
-top: 20px;
-`
+  position: absolute;
+  left: 20px;
+  top: 20px;
+`;
 
 export const Hero = styled.img`
-position: absolute;
-top: 26px;
-left: 36px;
-`
+  width: 308px;
+  height: 168px;
+
+  margin-bottom: 88px;
+`;
+
+export const UserAvatarWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+`;
+
+export const UserAvatarBorder = styled.div`
+  height: 80px;
+  width: 80px;
+  border: 8px;
+  border-radius: 50%;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 2.5777px 0px #fbf8ff inset,
+    0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.06),
+    0px -1.71846px 3.43693px 0px #ae7be3 inset;
+`;
+
+export const UserAvatarContainer = styled.div`
+  width: 62px;
+  /* height: 62px; */
+
+  position: absolute;
+  background: #5736a3;
+  border-radius: 50%;
+
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const UserAvatar = styled.img`
+  width: 100%;
+  border-radius: 50%;
+`;
+
+export const UserInfoWrapper = styled.ul`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 16px;
+margin-bottom: 20px;
+
+`;
+export const UserInfoItem = styled.li`
+  color: #ebd8ff;
+
+  font-size: 20px;
+  font-weight: 500;
+  font-style: normal;
+
+  text-transform: uppercase;
+
+`;
+
+export const FollowButton = styled.button`
+  display: block;
+  background: #ebd8ff;
+  box-shadow: 0px 3.43693px 3.43693px 0px rgba(0, 0, 0, 0.25);
+
+  width: 196px;
+  padding: 14px 28px;
+  border-radius: 10.3px;
+
+  border: none;
+
+  font-family: inherit;
+  font-size: 18px;
+  font-weight: 600;
+  text-transform: uppercase;
+
+  margin: 0 auto;
+  margin-bottom: 8px;
+  cursor: pointer;
+
+  ${({ isFollowing }) =>
+    isFollowing
+      ? css`
+          background: #5CD3A8;
+        `
+      : css`
+          background: #ebd8ff;
+        `}
+`;
